e17eddea293f42feb84eb369007ac91396e3ba7b
Bambi <leighasteele10@gmail.com>
2022-10-23 14:39:48 -0400

    Created user and thought routes in api

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+
+const {
+  getAllThoughts,
+  getThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+router.route("/").get(getAllThoughts).post(createThought);
+
+router
+  .route("/:thoughtId")
+  .get(getThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(addReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
